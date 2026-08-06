91b46eba34a9312bb4dd5ca37d818551edb913b4
CarlosEmiliano <carosmeilianosarmientosramos@gmail.com>
2026-08-06 15:14:01 -0600

    actualizar

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" class="min-h-screen bg-slate-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud de tareas</title>
     <link rel="stylesheet" href="assets/css/src/output.css">
 </head>
-<body class="min-h-screen bg-slate-100">
-    <h1 class="text-3xl font-bold text-slate-800">Crud de tareas</h1>
+<body class="m-0 min-h-screen bg-slate-100">
+    <h1 class="bg-warning p-4 text-center text-3xl font-bold text-slate-800">Crud de tareas</h1>
     <?php require_once "config/conexion.php"; ?>
 </body>
 </html>
