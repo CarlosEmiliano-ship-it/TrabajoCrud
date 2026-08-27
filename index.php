4f98cb7d30290f3ee7ece386215b362e090c1831
CarlosEmiliano <carosmeilianosarmientosramos@gmail.com>
2026-08-27 05:30:12 -0600

    Reciente

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $empleados = $empleadoModel->obtenerTodos();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Tareas</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/output.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
     <?php include_once __DIR__ . "/views/includes/header.php"; ?>
